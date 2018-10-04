8554ad0b1b82ecb6ac583a4206c0af2b23cec534
patdevincentis <devincentis529@gmail.com>
2018-10-04 15:30:59 -0400

    built library course component and laid it out

---
+++ src/components/library/libraryCourse.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class LibraryCourse extends Component {
+  render() {
+    return (
+      <div className='library__course'>
+        <label className="library-course__title"> Problem Solving </label>
+        {/*icon component*/}
+        {/*arrow component*/}
+        {/*action button component*/}
+        <div className='library-course__description'>
+          <label> Course Description </label>
+          <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pretium interdum risus eu egestas. Donec ac ultricies nunc. Vivamus ornare scelerisque risus ac facilisis</p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default LibraryCourse;
