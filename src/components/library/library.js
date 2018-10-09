df783abff46a0513645f225e1760988b0479c1e4
patdevincentis <devincentis529@gmail.com>
2018-10-08 21:31:53 -0400

    installed and connected redux devtools to application and used spread operator to set payload data in store

--- src/components/library/library.js
+++ src/components/library/library.js
@@ -5,8 +5,11 @@ import * as actions from '../../actions'
 import LibraryCourse from './libraryCourse'
 
 class Library extends Component {
-  render() {
+
+  componentDidMount() {
     this.props.fetchCourses()
+  }
+  render() {
     return (
       <div className='library'>
         <h1 className='library__title'>Course Library</h1>
